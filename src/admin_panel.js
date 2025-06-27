5126e6aa513908311e065b379d2f51fbadf04e33
Santharam <santharam141@gmail.com>
2025-06-27 18:15:33 +0530

    trying admin panel

---
+++ src/admin_panel.js
@@ -0,0 +1,113 @@
+import React from "react";
+import "./admin_panel.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "./navbars.css";
+import Getfit from "./Asset/GETFIT.png";
+import { HashLink } from "react-router-hash-link";
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
+const Adminpanel = () => {
+  return (
+    <>
+      <Navbar
+        className="customize-navbar d-flex flex-column flex-md-row justify-content-between align-items-center w-100 text-white"
+        expand="lg"
+      >
+        <Container>
+          <Navbar.Brand href="#phone" style={{ color: "white" }}>
+            📞12345678|✉️Getfit@gmail.com
+          </Navbar.Brand>
+          <Navbar.Brand style={{ color: "white" }}>Welcome Admin</Navbar.Brand>
+          <Navbar.Brand href="" style={{ color: "white" }}>
+            <a href="https://facebook.com">
+              <FaFacebook />
+            </a>
+            <a href="https://twitter.com">
+              <FaTwitter />
+            </a>
+            <a href="https://instagram.com">
+              <FaInstagram />
+            </a>
+            <a href="https://linkedin.com">
+              <FaLinkedin />
+            </a>
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      {/* first navbar */}
+
+      <Navbar className="customize-navbar2 sticky-top py-2 " expand="lg">
+        <Container fluid>
+          {" "}
+          {/*its make fluid to screen edge from navbar */}
+          <Navbar.Brand
+            className="d-flex align-items-center fs-5"
+            as={HashLink}
+            smooth
+            to="#car"
+          >
+            {" "}
+            <img
+              src={Getfit}
+              alt="logo"
+              width="40"
+              height="40"
+              className="me-2"
+            />
+            GETFIT
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" className="ms-0" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto ">
+              <Nav.Link
+                as={HashLink}
+                smooth
+                to="#Getstart"
+                className="hoverusingclass fs-6 "
+              >
+                View
+              </Nav.Link>
+              <Nav.Link
+                as={HashLink}
+                smooth
+                to="#fitness"
+                className="hoverusingclass fs-6"
+              >
+                Edit
+              </Nav.Link>
+
+              <Nav.Link
+                as={HashLink}
+                smooth
+                to="#equip"
+                className="hoverusingclass fs-6"
+              >
+                Create
+              </Nav.Link>
+              <Nav.Link
+                as={HashLink}
+                smooth
+                to="#Bmical"
+                className="hoverusingclass fs-6"
+              >
+                Delete
+              </Nav.Link>
+              <Nav.Link
+                as={HashLink}
+                smooth
+                to="#contactus"
+                className="hoverusingclass fs-6"
+              >
+                Logout
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      {/* second navbar */}
+      <hr className="line1"></hr>
+    </>
+  );
+};
+export default Adminpanel;
